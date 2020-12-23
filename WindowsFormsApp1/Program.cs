e265cd2b649a17cbcd9d00a0d69bb0eebe3105a0
namvgt <namvgt75@gmail.com>
2020-12-23 09:09:46 +0700

    yeah

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -18,7 +18,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form2());
+            Application.Run(new DangNhap());
         }
     }
 }
